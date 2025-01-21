4174ea4a62965cbe205d598045cc4a514f82fc64
millerding <mding022@uottawa.ca>
2025-01-21 17:44:18 -0500

    feat: add textloop

--- app/page.jsx
+++ app/page.jsx
@@ -42,7 +42,9 @@ export default function Home() {
           background: 'radial-gradient(at 90% 14%, rgba(16, 110, 232, 0.35) 0px, transparent 50%), radial-gradient(at 82% 69%, rgba(26, 10, 83, 0.35) 0px, transparent 50%), radial-gradient(at 59% 5%, rgba(181, 26, 0, 0.35) 0px, transparent 50%), #001524'
         }}>
         <h1 className="text-white text-3xl font-bold mt-10 ml-10 animate-0">Modern CSS Animations <a href="https://millerding.com" target="_blank"><button role="link" className="relative text-lg text-slate-300 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:translate-y-1 after:bg-slate-200 after:opacity-0 after:transition after:duration-150   after:ease-in-out hover:after:translate-y-0 hover:after:opacity-100">by Miller Ding</button></a></h1>
-        <h1 className="text-white text-xl font-normal mt-2 ml-10 animate-0 max-w-[45%]">A collection of modern, smooth, and simple to implement <span className="font-semibold">CSS animations</span> for your next <TextLoop><span>React app.</span><span>Next.js app.</span></TextLoop> No JS required, just paste it into your .css file.</h1>
+        <h1 className="text-white text-xl font-normal mt-2 ml-10 animate-0 max-w-[45%]">A collection of modern, smooth, and simple to implement <span className="font-semibold">CSS animations</span> for your next&nbsp;
+          <TextLoop interval={2000} delay={1000}><span>React app.</span><span>Next.js app.</span><span>Vue.js app.</span><span>Angular app.</span><span>Svelte app.</span><span>Ember.js app.</span><span>Bootstrap app.</span>
+          </TextLoop> No JS required, just paste it into your .css file.</h1>
         <div className="text-white text-xl font-semibold absolute right-10 top-10 flex gap-4 hover:underline underline-offset-8">
           <a href="#">
             <h1>Help</h1>
