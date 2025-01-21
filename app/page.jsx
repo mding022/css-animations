4965deb886d25cdda8b24e71c6221038d7a15126
millerding <mding022@uottawa.ca>
2025-01-21 18:47:48 -0500

    fix: index error

--- app/page.jsx
+++ app/page.jsx
@@ -56,7 +56,7 @@ export default function Home() {
         }}>
         <h1 className="text-white text-3xl font-bold mt-10 ml-10 animate-0">Modern CSS Animations <a href="https://millerding.com" target="_blank"><button role="link" className="relative text-lg text-slate-300 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:translate-y-1 after:bg-slate-200 after:opacity-0 after:transition after:duration-150   after:ease-in-out hover:after:translate-y-0 hover:after:opacity-100">by Miller Ding</button></a></h1>
         <h1 className="text-white text-xl font-normal mt-2 ml-10 animate-0 max-w-[45%]">A collection of modern, smooth, and simple to implement <span className="font-semibold">CSS animations</span> for your next&nbsp;
-          <TextLoop inline="true">{TEXTS[index]}</TextLoop> No JS required, just paste it into your .css file.
+          <TextLoop inline="true">{TEXTS[index % TEXTS.length]}</TextLoop> No JS required, just paste it into your .css file.
           <br />
           <a href="https://www.w3schools.com/css/css3_animations.asp" target='_blank' className="text-slate-400 font-semibold text-lg hover:underline underline-offset-4">
             Using keyframe animations in CSS
